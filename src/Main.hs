cceecf6897497795e1cb74430803bbb6076f78df
Alex Muscar <alex.muscar@ly.st>
2015-02-07 10:15:11 +0000

    Glorious initial commit.

---
+++ src/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Data.List
+
+main :: IO ()
+main = mapM_ putStrLn ["Lysa v0.1 (c) Alex Muscar 2015\n"
+                      , "Certainty of death, *small* chance of success... What are we waiting for?\n"]
